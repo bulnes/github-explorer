d5fe536e979a9736caaadd1f627b51cbee2791f2
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-07-12 09:27:54 -0300

    feat: creating first react component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+export default function Repositorylist() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repsitórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
