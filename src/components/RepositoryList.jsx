4dd697d6a44c46f4633a984dbb3b2a5e93f1faab
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-07-13 23:03:38 -0300

    feat: creating reporitory item component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,20 @@
+import RepositoryItem from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/bulnes'
+}
+
 export default function Repositorylist() {
   return (
     <section className="repository-list">
       <h1>Lista de repsitórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
